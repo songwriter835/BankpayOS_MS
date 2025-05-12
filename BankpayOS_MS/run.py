c0cb39e3eff9e7572e3a016f6fd22ac8f1d7e81d
writer <songwriter835@gmail.com>
2025-05-12 14:29:03 +0800

    updata data

--- BankpayOS_MS/run.py
+++ BankpayOS_MS/run.py
@@ -3,3 +3,4 @@ import BankpayOS_MS.common.utils as ut
 # 时间
 time_now = ut.get_time()
 pytest.main(['-vs', f'--html={ut.report_path}/{time_now}.html', f"{ut.case_path}",'--reruns', '2','--reruns-delay', '2'])
+
